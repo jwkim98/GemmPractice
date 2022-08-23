7a068fa9d2a8bc4852e86139f83a352d6bc67348
jinhwan-enerzai <jinhwan.shin@enerzai.com>
2022-08-23 13:32:38 +0900

    Added parallelization, unrolling and vertorization

--- Includes/KernelTutorial/Gemm.hpp
+++ Includes/KernelTutorial/Gemm.hpp
@@ -1,6 +1,24 @@
 #include <utility>
 
-using GemmKernel = void (*)(float*, float*, float*, float, float, std::size_t, std::size_t, std::size_t);
+using GemmKernel = void (*)(float* a, float* b, float* c, float alpha,
+                            float beta, std::size_t m,
+                            std::size_t n, std::size_t k);
 
-void GemmBasic(float* a, float* b, float* c, float alpha, float beta, std::size_t m, std::size_t n, std::size_t k);
-void GemmTiling(float* a, float* b, float* c, float alpha, float beta, std::size_t m, std::size_t n, std::size_t k);
+void GemmBasic(float* a, float* b, float* c, float alpha,
+               float beta, std::size_t m, std::size_t n,
+               std::size_t k);
+void GemmTiling(float* a, float* b, float* c, float alpha,
+                float beta, std::size_t m, std::size_t n,
+                std::size_t k);
+void GemmParallel(float* a, float* b, float* c, float alpha,
+                  float beta, std::size_t m, std::size_t n,
+                  std::size_t k);
+void GemmParallelTiled(float* a, float* b, float* c, float alpha,
+                       float beta, std::size_t m,
+                       std::size_t n, std::size_t k);
+void GemmUnrolled(float* a, float* b, float* c, float alpha,
+                  float beta, std::size_t m, std::size_t n,
+                  std::size_t k);
+void GemmVectorized(float* a, float* b, float* c, float alpha,
+                    float beta, std::size_t m, std::size_t n,
+                    std::size_t k);
--- Tests/UnitTests/SimpleTest.cpp
+++ Tests/UnitTests/SimpleTest.cpp
@@ -13,19 +13,24 @@ std::chrono::microseconds testGemm(GemmKernel kernel, std::size_t m, std::size_t
 {
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_real_distribution dist(-1.0f, 0.0f);
-    std::unique_ptr<float[]> arrA(reinterpret_cast<float*>(std::aligned_alloc(16, sizeof(float) * m * k)) /*new float[m*k]*/);
-    std::unique_ptr<float[]> arrB(reinterpret_cast<float*>(std::aligned_alloc(16, sizeof(float) * k * n)) /*new float[k*n]*/);
-    std::unique_ptr<float[]> arrC(reinterpret_cast<float*>(std::aligned_alloc(16, sizeof(float) * m * n)) /*new float[m*n]*/);
+    // std::uniform_real_distribution dist(-1.0f, 0.0f);
+    const auto dist = [](std::mt19937&) -> float { return 1.0; };
+    //std::unique_ptr<float[]> arrA(reinterpret_cast<float*>(std::aligned_alloc(16, sizeof(float) * m * k)) /*new float[m*k]*/);
+    //std::unique_ptr<float[]> arrB(reinterpret_cast<float*>(std::aligned_alloc(16, sizeof(float) * k * n)) /*new float[k*n]*/);
+    //std::unique_ptr<float[]> arrC(reinterpret_cast<float*>(std::aligned_alloc(16, sizeof(float) * m * n)) /*new float[m*n]*/);
+    float* arrA =
+        reinterpret_cast<float*>(_aligned_malloc(sizeof(float) * m * k, 64));
+    float* arrB =
+        reinterpret_cast<float*>(_aligned_malloc(sizeof(float) * k * m, 64));
+    float* arrC =
+        reinterpret_cast<float*>(_aligned_malloc(sizeof(float) * m * n, 64));
 
     for(std::size_t i = 0; i < m*k; ++i)
         arrA[i] = dist(gen);
     for(std::size_t i = 0; i < k*n; ++i)
         arrB[i] = dist(gen);
     for(std::size_t i = 0; i < m*n; ++i)
-    {
-                arrC[i] = dist(gen);
-    }
+        arrC[i] = dist(gen);
 
     blaze::DynamicMatrix<float, blaze::rowMajor> a(m, k, 0.0f);
     blaze::DynamicMatrix<float, blaze::rowMajor> b(k, n, 0.0f);
@@ -44,69 +49,53 @@ std::chrono::microseconds testGemm(GemmKernel kernel, std::size_t m, std::size_t
             c(i, j) = arrC[i*n + j];
 
     const auto t1 = std::chrono::system_clock::now();
-    kernel(arrA.get(), arrB.get(), arrC.get(), alpha, beta, m, n, k);
+    // kernel(arrA.get(), arrB.get(), arrC.get(), alpha, beta, m, n, k);
+    kernel(arrA, arrB, arrC, alpha, beta, m, n, k);
     const auto t2 = std::chrono::system_clock::now();
 
     const auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1);
-    std::cout<<"elapsed time : "<<elapsed.count()<<std::endl;
 
     c = alpha*a*b + beta*c;
-    auto mismatch = 0;
     for(std::size_t i = 0; i < m; ++i)
         for(std::size_t j = 0; j < n; ++j)
         {
-            if (std::abs(c(i, j) - arrC[i*n + j]) < std::numeric_limits<float>::epsilon() * 10)
+            if (std::abs(c(i, j) - arrC[i*n + j]) > std::numeric_limits<float>::epsilon() * 10000)
             {
-                ++mismatch;
-                // std::cout << c(i, j) << " vs " << arrC[i * n + j] << std::endl;
+                std::cout << c(i, j) << " vs " << arrC[i * n + j] << std::endl;
             }
         }
 
     return elapsed;
 }
 
-TEST_CASE("Gemm")
+TEST_CASE("parallel vs parallel_tiled")
 {
-    std::random_device rd;
-    std::default_random_engine engine(rd());
-    std::uniform_int_distribution<std::size_t> intDist(10, 20);
-    std::uniform_real_distribution<float> realDist(-5.0, 5.0);
-
-    for (auto i = 0; i < 10; ++i)
+    for (auto i = 0; i < 10; i++)
     {
-        std::size_t M = intDist(engine);
-        std::size_t N = intDist(engine);
-        std::size_t K = intDist(engine);
-        float alpha = realDist(engine);
-        float beta = realDist(engine);
+        auto elapsedP = testGemm(GemmParallel, 200, 200, 200, 1.0, 1.0);
+        auto elapsedPT = testGemm(GemmParallelTiled, 200, 200, 200, 1.0, 1.0);
 
-        std::cout << "#" << i << " - M: " << M << ", N: " << N << ", K: " << K << ", alpha: " << alpha << ", beta: " << beta << std::endl;
-
-        testGemm(GemmBasic, M, N, K, alpha, beta);
+        // std::cout << "#" << i << " Parallel: " << elapsedP.count() << "ms vs. ParallelTiled: " << elapsedPT.count() << "ms" << std::endl;
     }
 }
 
-TEST_CASE("Check calculation time of basic and tiled GEMM")
+TEST_CASE("tiled vs parallel vs unrolled")
 {
-    std::random_device rd;
-    std::default_random_engine engine(rd());
-    std::uniform_int_distribution<std::size_t> intDist(100, 200);
-    std::uniform_real_distribution<float> realDist(-5.0, 5.0);
-
-    for (auto i = 0; i < 10; ++i)
+    constexpr std::size_t N = 256;
+    
+    for (auto i = 0; i < 10; i++)
     {
-        std::size_t M = intDist(engine);
-        std::size_t N = intDist(engine);
-        std::size_t K = intDist(engine);
-        float alpha = realDist(engine);
-        float beta = realDist(engine);
+        auto elapsedB = testGemm(GemmBasic, N, N, N, 1.0, 1.0);
+        auto elapsedT = testGemm(GemmTiling, N, N, N, 1.0, 1.0);
+        auto elapsedP = testGemm(GemmParallelTiled, N, N, N, 1.0, 1.0);
+        auto elapsedU = testGemm(GemmUnrolled, N, N, N, 1.0, 1.0);
+        auto elapsedV = testGemm(GemmVectorized, N, N, N, 1.0, 1.0);
 
-        std::cout << "#" << i << " - M: " << M << ", N: " << N << ", K: " << K << ", alpha: " << alpha << ", beta: " << beta << std::endl;
-
-        const auto elapsedBasic = testGemm(GemmBasic, M, N, K, alpha, beta);
-        const auto elapsedTiled = testGemm(GemmTiling, M, N, K, alpha, beta);
-        const double d = (1.0 - static_cast<double>(elapsedTiled.count()) / static_cast<double>(elapsedBasic.count())) * 100.0;
-
-        std::cout << "#" << i << " - Tiling is " << d << " %(" << (elapsedBasic - elapsedTiled).count() << "ms) faster than basic." << std::endl;
+        std::cout << "#" << i << " Basic: " << elapsedB.count()
+                  << "ms vs. Tiled: " << elapsedT.count()
+                  << "ms vs. Parallel: " << elapsedP.count()
+                  << "ms vs. Unrolled: " << elapsedU.count()
+                  << "ms vs. Vectorized: " << elapsedV.count() << "ms"
+                  << std::endl;
     }
 }
